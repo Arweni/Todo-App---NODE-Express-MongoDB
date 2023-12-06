ee52e6bf9b73e2375049af962476f08381808b76
arweni <chad5059x1@yahoo.com>
2023-12-06 13:54:06 -0500

    updated nodemon vuln; changed mongoose no callback

--- server.js
+++ server.js
@@ -1,84 +1,91 @@
 //declare variables
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const PORT = 3000;
-const mongoose = require ('mongoose');
-require('dotenv').config()
-const TodoTask = require ('./models/todotask')
+const mongoose = require('mongoose');
+require('dotenv').config();
+const TodoTask = require('./models/todotask');
 
 //set middleware
-app.set("view engine",'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
-mongoose.connect(process.env.DB_CONNECTION,
-    {useNewUrlParser: true},
-    () => {console.log('Connected to MongoDB!')}
-)
+// mongoose.connect(process.env.DB_CONNECTION,
+//     {useNewUrlParser: true},
+//     () => {console.log('Connected to MongoDB!')}
+// )
+
+mongoose
+  .connect(process.env.DB_CONNECTION, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB: ', err);
+  });
 
 // GET METHOD
 app.get('/', async (req, res) => {
-    try{
-        TodoTask.find({}, (err, tasks) => {
-            res.render('index.ejs', {todoTasks: tasks})
-            })
-    } catch (err) {
-        if (err) return res.status(500).send(err)
-    }
-})
+  try {
+    TodoTask.find({}, (err, tasks) => {
+      res.render('index.ejs', { todoTasks: tasks });
+    });
+  } catch (err) {
+    if (err) return res.status(500).send(err);
+  }
+});
 
 //POST
-app.post('/', async(req, res) => {
-    const todoTask = new TodoTask(
-        {
-            title: req.body.title,
-            content: req.body.content
-        }
-    )
-    try {
-        await todoTask.save()
-        console.log(todoTask)
-        res.redirect("/")
-    } catch (err) {
-        if(err) return res.status(500).send(err)
-        res.redirect('/')        
-    }
-})
+app.post('/', async (req, res) => {
+  const todoTask = new TodoTask({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  try {
+    await todoTask.save();
+    console.log(todoTask);
+    res.redirect('/');
+  } catch (err) {
+    if (err) return res.status(500).send(err);
+    res.redirect('/');
+  }
+});
 
 //EDIT OR UPDATE
 app
-    .route("/edit/:id")
-    .get((req, res) => {
-        const id = req.params.id
-        TodoTask.find({}, (err, tasks) => {
-            res.render('edit.ejs', {
-                todoTasks:tasks, idTask: id})
-            })
-        })
-    .post((req, res) => {
-        const id = req.params.id
-        TodoTask.findByIdAndUpdate(
-            id,
-            {
-                title: req.body.title,
-                content: req.body.content
-            },
-            err => {
-                if (err) return res.status(500).send(err)
-                res.redirect('/')
-            }
-        )
-    })
+  .route('/edit/:id')
+  .get((req, res) => {
+    const id = req.params.id;
+    TodoTask.find({}, (err, tasks) => {
+      res.render('edit.ejs', {
+        todoTasks: tasks,
+        idTask: id,
+      });
+    });
+  })
+  .post((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndUpdate(
+      id,
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      (err) => {
+        if (err) return res.status(500).send(err);
+        res.redirect('/');
+      }
+    );
+  });
 
 //DELETE
-app
-    .route("/remove/:id")
-    .get((req, res) => {
-        const id = req.params.id
-        TodoTask.findByIdAndRemove(id, err => {
-            if (err) return res.status(500).send(err)
-            res.redirect('/')
-        })
-    })    
+app.route('/remove/:id').get((req, res) => {
+  const id = req.params.id;
+  TodoTask.findByIdAndRemove(id, (err) => {
+    if (err) return res.status(500).send(err);
+    res.redirect('/');
+  });
+});
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`))
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`));
